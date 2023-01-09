355b310ff0174a5ea85c017d0adb4ac6016608d6
Nikolinc <tsigulskynikita@gmail.com>
2023-01-09 15:28:53 +0500

    add init tg data

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -1,11 +1,15 @@
 
 // @ts-ignore
 const tg = window.Telegram.WebApp;
+console.log('tg',tg)
 export function useTelegram() {
 
     const onClose = () => {
         tg.close()
     }
+
+    const initData = tg.initData || '';
+    const initDataUnsafe = tg.initDataUnsafe || {};
 
     const onToggleButton = () => {
         if(tg.MainButton.isVisible) {
@@ -21,5 +25,7 @@ export function useTelegram() {
         tg,
         user: tg.initDataUnsafe?.user,
         queryId: tg.initDataUnsafe?.query_id,
+        initData,
+        initDataUnsafe,
     }
 }
