621e50c872b83b3f36e51a275010beb6e9119760
Nikolinc <tsigulskynikita@gmail.com>
2023-01-10 17:11:06 +0500

    add link to home page

--- src/component/error-page.tsx
+++ src/component/error-page.tsx
@@ -1,30 +1,28 @@
-import { useRouteError } from "react-router-dom";
-import {errorType} from "../types/error";
+import { Link, useRouteError } from "react-router-dom";
+import { IError } from "../types/error";
 
 export default function ErrorPage() {
-    // @ts-ignore
-    const error:errorType = useRouteError();
+  // @ts-ignore
+  const error: IError = useRouteError();
 
+  return (
+    <div className="grid h-screen px-4 bg-white place-content-center">
+      <div className="text-center">
+        <h1 className="font-black text-gray-200 text-9xl">{error.status}</h1>
 
+        <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          {error.statusText}
+        </p>
 
-    return (
-        <div className="grid h-screen px-4 bg-white place-content-center">
-            <div className="text-center">
-                <h1 className="font-black text-gray-200 text-9xl">{error.status}</h1>
+        <p className="mt-4 text-gray-500">We can't find that page.</p>
 
-                <p className="text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                    {error.statusText}
-                </p>
-
-                <p className="mt-4 text-gray-500">We can't find that page.</p>
-
-                <a
-                    href="#"
-                    className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
-                >
-                    Go Back Home
-                </a>
-            </div>
-        </div>
-    );
+        <Link
+          to="/"
+          className="inline-block px-5 py-3 mt-6 text-sm font-medium text-white bg-indigo-600 rounded hover:bg-indigo-700 focus:outline-none focus:ring"
+        >
+          Go Back Home
+        </Link>
+      </div>
+    </div>
+  );
 }
