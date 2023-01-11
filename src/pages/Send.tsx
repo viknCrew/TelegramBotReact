87599f266db7db864a92fd87e16ed6e3824b581d
Nikolinc <tsigulskynikita@gmail.com>
2023-01-11 12:32:17 +0500

    add pages

---
+++ src/pages/Send.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { useTelegram } from "../hooks/useTelegram";
+import { balance, Web3, web3 } from "../store/coinStore";
+
+export default function Send() {
+  const { user, initDataUnsafe } = useTelegram();
+  if (!user)
+    return (
+      <>
+        <div>user null</div>
+      </>
+    );
+  console.log("web3", web3);
+  console.log("Web3", Web3);
+  console.log("balance", balance);
+  return (
+    <div className="mx-8 grid lg:grid-cols-3  grid-cols-1">
+      <div>
+        <p>User</p>
+        <div className="flex  justify-between">
+          <p>First name :</p>
+          {user.first_name}
+        </div>
+        <div className="flex  justify-between">
+          <p>id :</p>
+          {user.id}
+        </div>
+        <div className="flex  justify-between">
+          <p>language_code :</p>
+          {user.language_code}
+        </div>
+        <div className="flex  justify-between">
+          <p>last_name</p>
+          {user.last_name}
+        </div>
+        <div className="flex  justify-between">
+          <p>username:</p>
+          {user.username}
+        </div>
+      </div>
+      <div>
+        <p>initDataUnsafe</p>
+        <div className="flex  justify-between">
+          <p>auth_date :</p>
+          {initDataUnsafe.auth_date}
+        </div>
+        <div className="flex  justify-between">
+          <p>hash :</p>
+          {initDataUnsafe.hash}
+        </div>
+        <div className="flex  justify-between">
+          <p>query_id:</p>
+          {initDataUnsafe.query_id}
+        </div>
+      </div>
+      <div>
+        <div className="flex  justify-between">
+          <p>web3</p>
+          {web3}
+        </div>
+        <div className="flex  justify-between">
+          <p>Web3</p>
+          {Web3}
+        </div>
+        <div className="flex  justify-between">
+          <p>Balance</p>
+          {balance}
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/Wallet.tsx
@@ -0,0 +1,155 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Wallet() {
+
+  const logo = require("../assets/LOGO.png");
+  const metaMask = require("../assets/MetaMask_Fox.png");
+
+  return (
+    <div className="grid grid-col-1 gap-6 w-full mx-[10px] mr-[10px]">
+      <div className="flex justify-center items-start ">
+        <div className="bg-[var(--tg-theme-link-color)] w-[250PX]  h-[200px] rounded-b-full flex justify-center items-center shadow-lg">
+          <img src={logo} />
+        </div>
+      </div>
+      <div className="Wallet w-full h-[150px] bg-[var(--tg-theme-secondary-bg-color)] rounded-xl shadow-lg">
+        <div className="flex justify-center items-start mt-[15px] font-black text-2xl">
+          Wallet
+        </div>
+        <div className="text-[#d7d7d7] font-smail text-xs ml-[30px]">
+          xc6D3720f6286C5173C94523b8b02d549c9933662
+        </div>
+        <div className="flex items-center ml-[30px]">
+          <img src={logo} className="w-[25px]" />
+          <p className="font-medium text-lg">20.4570015 TMY ≈</p>
+        </div>
+        <p className="text-[#d7d7d7] text-sm ml-[30px]">$ 30.3202 USDT</p>
+      </div>
+      <div className="grid gap-6 grid-cols-2">
+        <Link
+          to={"/send"}
+          className="bg-[var(--tg-theme-secondary-bg-color)] text-[var(--tg-theme-link-color)] py-[7px] flex items-center justify-center shadow-lg rounded-md"
+        >
+          <div className="grid grid-cols-1 m-0 p-0 text-xl font-semibold">
+            <div className="mx-auto w-[48px] h-[48px] rounded-full bg-[var(--tg-theme-button-color)] flex justify-center items-center">
+              <svg
+                className="fill-[var(--tg-theme-secondary-bg-color)]"
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="13"
+                viewBox="0 0 9 13"
+                fill="none"
+              >
+                <path
+                  d="M4.5 12L4.5 1"
+                  stroke="var(--tg-theme-secondary-bg-color)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M7.46153 3.96154L4.49999 1L1.53845 3.96154"
+                  stroke="var(--tg-theme-secondary-bg-color)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <p className="mx-auto flex justify-cent er"> Send</p>
+          </div>
+        </Link>
+        <Link
+          to={"/receive"}
+          className="bg-[var(--tg-theme-secondary-bg-color)] text-[var(--tg-theme-link-color)] py-[7px] flex items-center justify-center shadow-lg rounded-md"
+        >
+          <div className="grid grid-cols-1 m-0 p-0 text-xl font-semibold">
+            <div className="mx-auto w-[48px] h-[48px] rounded-full bg-[var(--tg-theme-button-color)] flex justify-center items-center">
+              <svg
+                className="fill-[var(--tg-theme-secondary-bg-color)]"
+                xmlns="http://www.w3.org/2000/svg"
+                width="9"
+                height="13"
+                viewBox="0 0 9 13"
+                fill="none"
+              >
+                <path
+                  d="M4.5 1L4.5 12"
+                  stroke="var(--tg-theme-secondary-bg-color)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M1.53847 9.03846L4.50001 12L7.46155 9.03846"
+                  stroke="var(--tg-theme-secondary-bg-color)"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </div>
+            <p className="mx-auto flex justify-center"> Receive</p>
+          </div>
+        </Link>
+      </div>
+      <div className="bg-[var(--tg-theme-secondary-bg-color)] rounded-xl shadow-lg w-full h-[400px] grid grid-cols-1">
+        <p className="text-2xl font-bold flex justify-center mt-2]"> History</p>
+        <div className="bg-[var(--tg-theme-secondary-bg-color)] rounded-xl shadow-2xl w-full h-[100px] grid grid-cols-1 px-4">
+          <div className="flex">
+            <img src={metaMask} />
+            <div>
+              <p className="text-lg ">Replenishment via metamask</p>
+              <p className="text-xs text-var(--tg-theme-hint-color)] font-thin">
+                xbc5EFF393893a0AFDd0e7b89FA0DD2DC7d913423
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-xs text-[var(--tg-theme-hint-color)] font-thin">
+              December 31 at 18:43
+            </p>
+            <p className="text-[#00FCDE] items-start"> + 20.4570015 TMY </p>
+          </div>
+        </div>
+        <div className="bg-var(--tg-theme-secondary-bg-color)] rounded-xl shadow-2xl w-full h-[100px] grid grid-cols-1 px-4">
+          <div className="flex">
+            <img src={metaMask} />
+            <div>
+              <p className="text-lg ">Replenishment via metamask</p>
+              <p className="text-xs text-[var(--tg-theme-hint-color)] font-thin">
+                xbc5EFF393893a0AFDd0e7b89FA0DD2DC7d913423
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-xs text-[var(--tg-theme-hint-color)] font-thin">
+              December 31 at 18:43
+            </p>
+            <p className="text-[#00FCDE] items-start"> + 20.4570015 TMY </p>
+          </div>
+        </div>
+        <div className="bg-[var(--tg-theme-secondary-bg-color)] rounded-xl shadow-2xl w-full h-[100px] grid grid-cols-1 px-4">
+          <div className="flex">
+            <img src={metaMask} />
+            <div>
+              <p className="text-lg ">Replenishment via metamask</p>
+              <p className="text-xs text-[var(--tg-theme-hint-color)] font-thin">
+                xbc5EFF393893a0AFDd0e7b89FA0DD2DC7d913423
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-between">
+            <p className="text-xs text-[var(--tg-theme-hint-color)] font-thin">
+              December 31 at 18:43
+            </p>
+            <p className="text-[#00FCDE] items-start"> + 20.4570015 TMY </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Wallet;
