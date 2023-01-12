4b45ed72eda204e90d0842458215546ce81978bd
Nikolinc <tsigulskynikita@gmail.com>
2023-01-13 01:02:22 +0500

    add showScanQrPopup

--- src/hooks/useTelegram.ts
+++ src/hooks/useTelegram.ts
@@ -2,6 +2,17 @@ import { IInitDataUnsafe, IUser } from "../types/telegram";
 
 // @ts-ignore
 const tg = window.Telegram.WebApp;
+
+const DemoApp = {
+  initData: tg.initData || "",
+  initDataUnsafe: tg.initDataUnsafe || {},
+  MainButton: tg.MainButton,
+
+  showAlert: function (message: any) {
+    tg.showAlert(message);
+  },
+};
+
 export function useTelegram() {
   const onClose = () => {
     tg.close();
@@ -18,10 +29,37 @@ export function useTelegram() {
     }
   };
 
+  const showScanQrPopup = (links_only: any) => {
+    tg.showScanQrPopup(
+      {
+        text: links_only ? "with any link" : "for test purposes",
+      },
+      function (text: { toString: () => string }) {
+        if (links_only) {
+          var lower_text = text.toString().toLowerCase();
+          if (
+            lower_text.substr(0, 7) == "http://" ||
+            lower_text.substr(0, 8) == "https://"
+          ) {
+            setTimeout(function () {
+              tg.openLink(text);
+            }, 50);
+            return true;
+          }
+        } else {
+          DemoApp.showAlert(text);
+          return true;
+        }
+      }
+    );
+  };
+
   return {
     onClose,
     onToggleButton,
+    showScanQrPopup,
     tg,
+    DemoApp,
     user,
     initDataUnsafe,
   };
