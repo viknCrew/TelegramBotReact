c21f4dc5806cc6cebd377e8ab187f7523d2b3326
Nikolinc <tsigulskynikita@gmail.com>
2023-01-16 17:26:53 +0500

    add balance

--- src/store/coinStore.ts
+++ src/store/coinStore.ts
@@ -1,5 +1,21 @@
+import { WalletEvent } from "./userStire";
+
 export const Web3 = require("web3");
 export const web3 = new Web3("https://node1.tmyblockchain.org/rpc");
-export const balance = web3.eth.getBalance(
-  "0xc6D3720f6286C5173C94523b8b02d549c9933662"
-);
+
+export let balance: any;
+
+export default async function CheckBalance() {
+  try {
+    web3.eth
+      .getBalance("0xc6D3720f6286C5173C94523b8b02d549c9933662")
+      .then((balanceInWei: any) => {
+        balance = web3.utils.fromWei(balanceInWei);
+        console.log("Balance in wei:", balanceInWei);
+        console.log("Balance in ETH:", balance);
+        WalletEvent(balance);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+}
