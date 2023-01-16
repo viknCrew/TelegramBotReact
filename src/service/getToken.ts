84f2962d6e5056f46f4e29cdd980055ad30be664
Nikolinc <tsigulskynikita@gmail.com>
2023-01-16 22:26:59 +0500

    add log

--- src/service/getToken.ts
+++ src/service/getToken.ts
@@ -1,10 +1,15 @@
 import HmacSHA256 from "crypto-js/hmac-sha256";
 import Hex from "crypto-js/enc-hex";
 
+function transformInitData(initData: any) {
+  return Object.fromEntries(new URLSearchParams(initData));
+}
+
 export default async function checkWebAppSignature(
   token: string,
-  initData: string
+  initData: any
 ) {
+  const encoder = new TextEncoder();
   const checkString = Object.keys(initData)
     .filter((key: any) => key !== "hash")
     .map((key: any) => `${key}=${initData[key]}`)
@@ -13,18 +18,37 @@ export default async function checkWebAppSignature(
 
   console.log("computed string:", checkString);
 
-  const q = new URLSearchParams(initData);
-  // Extract the hash
-  const hash = q.get("hash");
-  q.delete("hash");
-  const v = Array.from(q.entries());
-  v.sort(([aN, aV], [bN, bV]) => aN.localeCompare(bN));
-  const data_chack_string = v.map(([n, v]) => `${n}=${v}`).join("\n");
+  const secretKey = await crypto.subtle.importKey(
+    "raw",
+    encoder.encode("WebAppData"),
+    { name: "HMAC", hash: "SHA-256" },
+    true,
+    ["sign"]
+  );
+  const secret = await crypto.subtle.sign(
+    "HMAC",
+    secretKey,
+    encoder.encode(token)
+  );
+  const signatureKey = await crypto.subtle.importKey(
+    "raw",
+    secret,
+    { name: "HMAC", hash: "SHA-256" },
+    true,
+    ["sign"]
+  );
+  const signature = await crypto.subtle.sign(
+    "HMAC",
+    signatureKey,
+    encoder.encode(checkString)
+  );
 
-  var secret_key = HmacSHA256(token, "WebAppData").toString(Hex);
-  var key = HmacSHA256(data_chack_string, secret_key).toString(Hex);
-  console.log("secret_key", secret_key);
-  console.log("key", key);
+  const hex = [...new Uint8Array(signature)]
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
 
-  return key === hash;
+  console.log("original hash:", initData.hash);
+  console.log("computed hash:", hex);
+
+  return initData.hash === hex;
 }
