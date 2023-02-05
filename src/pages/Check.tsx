2e9e01bcc1e32f4d499d2532300be2e39aa7d780
Nikolinc <tsigulskynikita@gmail.com>
2023-02-05 22:09:42 +0500

    update button

--- src/pages/Check.tsx
+++ src/pages/Check.tsx
@@ -42,8 +42,8 @@ export default function Check() {
   }
 
   return (
-    <div className="flex justify-center w-full h-[98vh] bg-[var(--tg-theme-bg-color)] rounded-xl">
-      <div className="grid grid-cols-1 ">
+    <div className="flex justify-center w-full h-[98vh] rounded-xl">
+      <div className="grid grid-cols-1 w-96 bg-[var(--tg-theme-bg-color)]">
         <div className="text-center text-xl font-bold my-8 bg-[var(--tg-theme-bg-color)] flex items-center ">
           <div className="grid col-span-1">
             {" "}
@@ -75,7 +75,7 @@ export default function Check() {
         <div className="flex justify-center">
           <Link
             to="/"
-            className="rounded-xl my-1 mx-6 text-sm font-medium bg-[var(--tg-theme-link-color)] text-center text-[var(--tg-theme-bg-color)] "
+            className="rounded-xl my-1 mx-6 text-sm  w-56 h-8 mt-6  font-bold bg-[var(--tg-theme-link-color)] text-center text-[var(--tg-theme-bg-color)] "
           >
             Go Back Home
           </Link>
