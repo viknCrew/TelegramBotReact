4b94a772d4545e85ff6237bc0f212a131449a60a
Nikolinc <tsigulskynikita@gmail.com>
2023-02-06 20:20:11 +0500

    fix ui check

--- src/pages/Check.tsx
+++ src/pages/Check.tsx
@@ -61,65 +61,67 @@ export default function Check(props: IProps) {
   }
 
   return (
-    <div className="flex justify-center w-full h-[85vh] rounded-xl">
-      <div className="grid grid-cols-1 w-80 mt-16 bg-[var(--tg-theme-bg-color)] rounded-xl  shadow-lg">
-        <div className=" flex justify-center">
-          <div
-            className="trigger w-full
+    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+      <div className="flex justify-center w-full h-[85vh] rounded-xl">
+        <div className="grid grid-cols-1 w-80 mt-16 bg-[var(--tg-theme-bg-color)] rounded-xl  shadow-lg">
+          <div className=" flex justify-center">
+            <div
+              className="trigger w-full
            flex justify-center ml-2 top-[-12px] ]"
-          >
-            <Tick size={50} />
-          </div>
-          {/* <div className="сlose">1</div> */}
-        </div>
-        <div className="text-center text-sm font-bold bg-[var(--tg-theme-bg-color)] flex items-center  shadow-lg rounded-lg ">
-          <div className="grid col-span-1">
-            {" "}
-            <div className="">Sender's address</div>
-            <button
-              className="font-normal text-center text-xs text-[var(--tg-theme-link-color)] mx-10"
-              onClick={() => copyMyWallet()}
             >
-              <p>{placeInCenter(wallet, " ")}</p>
-            </button>
+              <Tick size={50} />
+            </div>
+            {/* <div className="сlose">1</div> */}
           </div>
-        </div>
-        <div className="text-center text-sm font-bold bg-[var(--tg-theme-bg-color)] flex items-center  shadow-lg rounded-lg ">
-          <div className="grid col-span-1">
-            {" "}
-            <div className="">Tokens transferred to the wallet</div>
-            <button
-              className="font-normal text-xs text-center text-[var(--tg-theme-link-color)] mx-10"
-              onClick={() => copyAddressTrans()}
-            >
-              <p>{placeInCenter(props.address, " ")}</p>
-            </button>
+          <div className="text-center text-sm font-bold bg-[var(--tg-theme-bg-color)] flex items-center  shadow-lg rounded-lg ">
+            <div className="grid col-span-1">
+              {" "}
+              <div className="">Sender's address</div>
+              <button
+                className="font-normal text-center text-xs text-[var(--tg-theme-link-color)] mx-10"
+                onClick={() => copyMyWallet()}
+              >
+                <p>{placeInCenter(wallet, " ")}</p>
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="text-center text-sm font-bold bg-[var(--tg-theme-bg-color)] flex items-center  shadow-lg rounded-lg ">
-          <div className="grid col-span-1">
-            {" "}
-            <div className="">Transaction</div>
-            <button
-              className="font-normal text-xs text-center text-[var(--tg-theme-link-color)] mx-10"
-              onClick={() => copyTransaction()}
-            >
-              <p>{placeInCenter(props.transaction, " ")}</p>
-            </button>
+          <div className="text-center text-sm font-bold bg-[var(--tg-theme-bg-color)] flex items-center justify-center shadow-lg rounded-lg ">
+            <div className="grid col-span-1">
+              {" "}
+              <div className="">Tokens transferred to the wallet</div>
+              <button
+                className="font-normal text-xs text-center text-[var(--tg-theme-link-color)] mx-10"
+                onClick={() => copyAddressTrans()}
+              >
+                <p>{placeInCenter(props.address, " ")}</p>
+              </button>
+            </div>
           </div>
-        </div>
-        <div className=" flex justify-center w-full bg-[var(--tg-theme-bg-color)] h-20 items-center rounded-xl shadow-lg text-center text-sm font-medium ">
-          Transfer amount:
-          {`  ${props.amount} TMY`}
-        </div>
+          <div className="text-center text-sm font-bold bg-[var(--tg-theme-bg-color)] flex items-center  shadow-lg rounded-lg ">
+            <div className="grid col-span-1">
+              {" "}
+              <div className="">Transaction</div>
+              <button
+                className="font-normal text-xs text-center text-[var(--tg-theme-link-color)] mx-10"
+                onClick={() => copyTransaction()}
+              >
+                <p>{placeInCenter(props.transaction, " ")}</p>
+              </button>
+            </div>
+          </div>
+          <div className=" flex justify-center w-full bg-[var(--tg-theme-bg-color)] h-20 items-center rounded-xl shadow-lg text-center text-sm font-medium ">
+            Transfer amount:
+            {`  ${props.amount} TMY`}
+          </div>
 
-        <div className="flex justify-center">
-          <button
-            onClick={() => closeModal()}
-            className="rounded-lg my-1 mx-6 text-sm  w-56 h-8 mt-3  font-bold bg-[var(--tg-theme-link-color)] text-center text-[var(--tg-theme-bg-color)] shadow-lg flex  justify-center items-center"
-          >
-            Go Back Home
-          </button>
+          <div className="flex justify-center">
+            <button
+              onClick={() => closeModal()}
+              className="rounded-lg my-1 mx-6 text-sm  w-56 h-8 mt-3  font-bold bg-[var(--tg-theme-link-color)] text-center text-[var(--tg-theme-bg-color)] shadow-lg flex  justify-center items-center"
+            >
+              Go Back Home
+            </button>
+          </div>
         </div>
       </div>
     </div>
