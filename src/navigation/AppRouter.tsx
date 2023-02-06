4cc19d58d9a470f4edec10ab5809cdf322b09928
Nikolinc <tsigulskynikita@gmail.com>
2023-02-06 18:31:08 +0500

    log error

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -5,18 +5,15 @@ import Send from "../pages/Send";
 import Receive from "../pages/Receive";
 import Trancsation from "../pages/transation";
 import Remittance from "../pages/Remittance";
-import Check from "../pages/Check";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Wallet />,
-    errorElement: <ErrorPage />,
   },
   {
     path: "/trancsation/:id",
     element: <Trancsation />,
-    errorElement: <ErrorPage />,
   },
   {
     path: "/remittance/:address",
@@ -29,6 +26,6 @@ export const router = createBrowserRouter([
   {
     path: "/receive",
     element: <Receive />,
-    errorElement: <ErrorPage />,
+    // errorElement: <ErrorPage />,
   },
 ]);
