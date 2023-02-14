c236cb10fb28db3e118eaa70b91252f3908520de
Nikolinc <tsigulskynikita@gmail.com>
2023-02-15 00:24:12 +0500

    upload  loader transation

--- src/component/loaderTransation.tsx
+++ src/component/loaderTransation.tsx
@@ -4,7 +4,7 @@ export default function LoaderTransation() {
       <div className="grid grid-col-1 mt-10 gap-6 w-[90%]">
         <div className=" h-[180px] bg-[var(--tg-theme-bg-color)] rounded-xl shadow-lg pb-8 w-full ">
           <div className="m-8 text-lg  font-medium leading-4 ">
-            <div className="h-[50px] w-[50px]  bg-[#FF3A3A]  rounded-full dark:bg-[#00FCDE]" />
+            <div className="h-[50px] w-[50px]  bg-[#00FCDE]  rounded-full dark:bg-[var(--tg-theme-secondary-bg-color)]" />
 
             <div className="my-4 w-[100px] h-[1.75rem] bg-[var(--tg-theme-hint-color)]  rounded-full dark:bg-[var(--tg-theme-secondary-bg-color)]"></div>
             <div className="font-light items-end bg-[var(--tg-theme-hint-color)]  rounded-full dark:bg-[var(--tg-theme-secondary-bg-color)] h-[1.25rem] w-[200px]"></div>
