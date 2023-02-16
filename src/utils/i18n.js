5157d0f04c3b09d9065cfd22e0b129936d56c4aa
Nikolinc <tsigulskynikita@gmail.com>
2023-02-16 15:39:40 +0500

    Added language dependence on telegram

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -2,13 +2,14 @@ import i18n from "i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
+const { tg } = useTelegram();
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: "en",
+    fallbackLng: tg.initDataUnsafe.user.language_code,
     debug: true,
     detection: {
       order: ["queryString", "cookie"],
