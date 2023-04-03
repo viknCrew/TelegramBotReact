df39cc3d412908c81381c8b3c49fb427a2761825
Nikolinc <tsigulskynikita@gmail.com>
2023-04-04 03:59:17 +0500

    add router exchanger

--- src/navigation/AppRouter.tsx
+++ src/navigation/AppRouter.tsx
@@ -5,6 +5,7 @@ import Send from "../pages/Send";
 import Receive from "../pages/Receive";
 import Trancsation from "../pages/transation";
 import Remittance from "../pages/Remittance";
+import Exchanger from "../pages/Exchanger";
 
 export const router = createBrowserRouter([
   {
@@ -30,6 +31,11 @@ export const router = createBrowserRouter([
   {
     path: "/receive",
     element: <Receive />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/exchanger",
+    element: <Exchanger />,
     errorElement: <ErrorPage />,
   },
 ]);
