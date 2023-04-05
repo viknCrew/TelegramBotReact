786f29fe8c7509751dffea5ae170e9ed84ed8918
Nikolinc <tsigulskynikita@gmail.com>
2023-04-06 01:45:27 +0500

    add filter

--- src/pages/Exchanger.tsx
+++ src/pages/Exchanger.tsx
@@ -20,7 +20,18 @@ export default function Exchanger() {
             <div className="bg-[#3947C9] py-1 px-3  rounded-full">TMY</div>
             <div className="bg-[#2D8C0B] py-1 px-3  rounded-full">1200</div>
           </div>
-          <button>
+
+          <input
+            type="checkbox"
+            name="hamburger"
+            id="hamburger"
+            className="peer"
+            hidden
+          />
+          <label
+            htmlFor="hamburger"
+            className="peer-checked:hamburger block relative z-20 p-6 -mr-6 cursor-pointer lg:hidden"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               height="28"
@@ -33,7 +44,61 @@ export default function Exchanger() {
                 d="M400 816v-60h160v60H400ZM240 606v-60h480v60H240ZM120 396v-60h720v60H120Z"
               />
             </svg>
-          </button>
+          </label>
+
+          <div className="peer-checked:translate-x-0 fixed inset-0 w-[calc(100%-4.5rem)] translate-x-[-100%] bg-white border-r shadow-xl transition duration-300 lg:border-r-0 lg:w-auto lg:static lg:shadow-none lg:translate-x-0">
+            <div className="flex flex-col h-full justify-between lg:items-center lg:flex-row">
+              <ul className="px-6 pt-32 text-gray-700 space-y-8 md:px-12 lg:space-y-0 lg:flex lg:space-x-12 lg:pt-0">
+                <li>
+                  <a
+                    href="#"
+                    className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:bg-cyan-100"
+                  >
+                    <span className="relative text-cyan-800">Home</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0 before:bg-cyan-100 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100"
+                  >
+                    <span className="relative group-hover:text-cyan-800">
+                      Services
+                    </span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0 before:bg-cyan-100 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100"
+                  >
+                    <span className="relative group-hover:text-cyan-800">
+                      Portfolio
+                    </span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    className="group relative before:absolute before:inset-x-0 before:bottom-0 before:h-2 before:origin-right before:scale-x-0 before:bg-cyan-100 before:transition before:duration-200 hover:before:origin-left hover:before:scale-x-100"
+                  >
+                    <span className="relative group-hover:text-cyan-800">
+                      About us
+                    </span>
+                  </a>
+                </li>
+              </ul>
+
+              <div className="border-t py-8 px-6 md:px-12 md:py-16 lg:border-t-0 lg:border-l lg:py-0 lg:pr-0 lg:pl-6">
+                <a
+                  href="#"
+                  className="block px-6 py-3 rounded-full bg-gradient-to-r from-sky-600 to-cyan-400 text-center text-white"
+                >
+                  Get started
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
         {advert.map((ad: IAdvert) => {
           return (
