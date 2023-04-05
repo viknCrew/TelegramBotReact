67acf20a1e7f57f1f96cd50a39456b15bc1faa8e
Nikolinc <tsigulskynikita@gmail.com>
2023-04-06 00:45:31 +0500

    add advert interface

---
+++ src/types/advert.ts
@@ -0,0 +1,13 @@
+
+export interface IAdvert{
+  image:string,
+  nickname:string,
+  available:number,
+  limits:number,
+  paymentMethod:string,
+  currency:string
+  crypto:string
+  numberAdventer:string
+  payWithin:string   
+  comment:string    
+}
