f444a1f3dde87394161a34dcd5ef2957874b40af
Nikolinc <tsigulskynikita@gmail.com>
2023-04-06 20:38:48 +0500

    update Purchase

--- src/pages/Purchase.tsx
+++ src/pages/Purchase.tsx
@@ -84,7 +84,7 @@ export default function Purchase() {
                 maxLength={6}
                 name="price"
                 id="price"
-                className={` border-none bg-[var(--tg-theme-bg-color)] focus:border-none text-3xl  sm:text-sm  mx-4 ${
+                className={` border-none bg-[var(--tg-theme-bg-color)] focus:border-none text-3xl  sm:text-sm  mx-8 ${
                   tg.MainButton.isActive
                     ? "text-[var(--tg-theme-button-color)]"
                     : "text-[#FF3A3A]"
