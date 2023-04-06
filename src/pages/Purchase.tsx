aff554a24d3d5bb40cc20cd0a3e46e5770bba3e0
Nikolinc <tsigulskynikita@gmail.com>
2023-04-06 19:27:22 +0500

    update Purchase

--- src/pages/Purchase.tsx
+++ src/pages/Purchase.tsx
@@ -3,22 +3,27 @@ import { GlobalStore } from "../store";
 import { useUnit } from "effector-react";
 import { IAdvert } from "../types/advert";
 import { useTelegram } from "../hooks/useTelegram";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Purchase() {
   const params = useParams();
   const { AdvertStore } = GlobalStore();
   const advert: IAdvert[] = useUnit(AdvertStore.store);
   const { tg } = useTelegram();
+  const [text, setText] = useState<string>("");
+  const searchInput: React.MutableRefObject<any> = useRef<any>();
 
   useEffect(() => {
     AdvertStore.event(String(params.id));
     tg.BackButton.show();
   }, []);
-
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const limit: number = 6;
+    setText(e.target.value.slice(0, limit));
+  };
   const navigate = useNavigate();
   const onBack = useCallback(() => {
-    navigate("/");
+    navigate("/exchanger");
   }, []);
 
   useEffect(() => {
@@ -32,7 +37,7 @@ export default function Purchase() {
     <div className="flex justify-center">
       <div className="grid grid-col-1 mt-10 gap-6 w-[90%] ">
         <div className="w-full py-3 bg-[var(--tg-theme-bg-color)] rounded-xl shadow-lg sticky top-0">
-          <div className="flex gap-3 items-center mx-4 mt-4 mb-2">
+          <div className="flex gap-3 items-center mx-4 mt-4 mb-1">
             <img
               src={advert[0].image}
               alt="avatar"
@@ -42,10 +47,43 @@ export default function Purchase() {
               You buy from {advert[0].nickname}
             </p>
           </div>
-          {advert[0].numberAdventer}
+          <div className="flex gap-3 items-center mx-4 mt-1 mb-2">
+            {" "}
+            {advert[0].numberAdventer}
+          </div>
         </div>
         <div className="w-full py-3 bg-[var(--tg-theme-bg-color)] rounded-xl shadow-lg sticky top-0">
-          
+          <div className="flex items-center">
+            <div className="relative mt-1 rounded-md shadow-sm">
+              <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                ₽
+              </div>
+              <input
+                type="number"
+                ref={searchInput}
+                maxLength={6}
+                name="price"
+                id="price"
+                className={`w-full border-none focus:border-none text-3xl bg-[var(--tg-theme-secondary-bg-color)] pr-12 sm:text-sm pl-12 ${
+                  tg.MainButton.isActive
+                    ? "text-[var(--tg-theme-button-color)]"
+                    : "text-[#FF3A3A]"
+                }  flex flex-grow-1s`}
+                placeholder="0.00"
+                value={text}
+                onChange={handleChange}
+                autoFocus
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center">
+                <div
+                  id="currency"
+                  className="h-full rounded-md border-transparent bg-transparent py-0 pl-2 pr-7 sm:text-sm flex items-center text-[var(--tg-theme-button-color)]"
+                >
+                  RUB
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
