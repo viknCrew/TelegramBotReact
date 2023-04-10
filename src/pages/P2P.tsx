85746a41c7dbe488401a36c1c94cd4ac2e4dfb05
Nikolinc <tsigulskynikita@gmail.com>
2023-04-10 22:52:57 +0500

    update  history p2p

--- src/pages/P2P.tsx
+++ src/pages/P2P.tsx
@@ -164,7 +164,7 @@ export default function P2P() {
             <div className="h-4 w-[98%] ">
               <Link
                 to={`/`}
-                className="bg-[var(--tg-theme-secondary-bg-color)] rounded-xl shadow-2xl w-[98%] p-2 grid grid-cols-1 px-4 my-3"
+                className="bg-[var(--tg-theme-secondary-bg-color)] rounded-xl shadow-2xl p-2 grid grid-cols-1 px-4 mx-4  my-3"
               >
                 <div className="flex gap-3 items-center mx-4 mt-2 mb-2 border-b-2 border-[var(--tg-theme-text-color)]">
                   <p className="text-xs font-bold align-text-top flex gap-2 bg-[00FCDE]">
