de4f8ba007e4fe0db7a1d25385d649ce18f34162
Nikolinc <tsigulskynikita@gmail.com>
2023-04-11 22:17:51 +0500

    update DropdownTabel

--- src/pages/CreateOrder.tsx
+++ src/pages/CreateOrder.tsx
@@ -124,14 +124,13 @@ export default function CreateOrder() {
 function DropdownTabel(props: { text: string }) {
   return (
     <td className="whitespace-nowrap px-4 flex gap-2 items-center">
-      {props.text}
       <button
         id="dropdownDefaultButton"
         data-dropdown-toggle="dropdown"
-        className="text-white  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center  dark:focus:ring-blue-800"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
-        {props.text}{" "}
+        {props.text}
         <svg
           className="w-4 h-4 ml-2"
           aria-hidden="true"
