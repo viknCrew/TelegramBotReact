604342061f36169cf550f463c84a704c3f93cf1e
Nikolinc <tsigulskynikita@gmail.com>
2023-04-11 22:03:13 +0500

    update DropdownTabel

--- src/pages/CreateOrder.tsx
+++ src/pages/CreateOrder.tsx
@@ -124,19 +124,73 @@ export default function CreateOrder() {
 function DropdownTabel(props: { text: string }) {
   return (
     <td className="whitespace-nowrap px-4 flex gap-2 items-center">
-      TMY{" "}
-      <svg
-        width="10"
-        height="10"
-        viewBox="0 0 19 19"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
+      {props.text}
+      <button
+        id="dropdownDefaultButton"
+        data-dropdown-toggle="dropdown"
+        className="text-white  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center  dark:focus:ring-blue-800"
+        type="button"
       >
-        <path
-          d="M1.62297 6.01666L2.75109 4.90833L9.53963 11.6969L16.3282 4.90833L17.4563 6.01666L9.53963 13.9333L1.62297 6.01666Z"
-          fill="white"
-        />
-      </svg>
+        {props.text}{" "}
+        <svg
+          className="w-4 h-4 ml-2"
+          aria-hidden="true"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M19 9l-7 7-7-7"
+          ></path>
+        </svg>
+      </button>
+
+      <div
+        id="dropdown"
+        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+      >
+        <ul
+          className="py-2 text-sm text-gray-700 dark:text-gray-200"
+          aria-labelledby="dropdownDefaultButton"
+        >
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Settings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Earnings
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+            >
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </div>
     </td>
   );
 }
