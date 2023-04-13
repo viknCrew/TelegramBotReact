7ac4e7ec2f3b06c025538579fe6b3a9c45237436
Nikolinc <tsigulskynikita@gmail.com>
2023-04-14 01:33:27 +0500

    update create order

--- src/pages/CreateOrder.tsx
+++ src/pages/CreateOrder.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { useTelegram } from "../hooks/useTelegram";
 
@@ -121,76 +121,42 @@ export default function CreateOrder() {
   );
 }
 
-function DropdownTabel(props: { text: string }) {
+const DropdownTabel = (props: { text: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <>
+    <div className="relative">
+      {/* Trigger Button */}
       <button
-        id="dropdownHoverButton"
-        data-dropdown-toggle="dropdownHover"
-        data-dropdown-trigger="click"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        type="button"
+        className="block bg-gray-200 text-gray-700 font-semibold py-2 px-4 rounded-md"
+        onClick={() => setIsOpen(!isOpen)}
       >
-        Dropdown hover{" "}
-        <svg
-          className="w-4 h-4 ml-2"
-          aria-hidden="true"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 9l-7 7-7-7"
-          ></path>
-        </svg>
+        Dropdown
       </button>
 
-      <div
-        id="dropdownHover"
-        className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-      >
-        <ul
-          className="py-2 text-sm text-gray-700 dark:text-gray-200"
-          aria-labelledby="dropdownHoverButton"
-        >
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Dashboard
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Settings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Earnings
-            </a>
-          </li>
-          <li>
-            <a
-              href="#"
-              className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-            >
-              Sign out
-            </a>
-          </li>
-        </ul>
-      </div>
-    </>
+      {/* Dropdown Menu */}
+      {isOpen && (
+        <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg">
+          <a
+            href="#"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+          >
+            Item 1
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+          >
+            Item 2
+          </a>
+          <a
+            href="#"
+            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+          >
+            Item 3
+          </a>
+        </div>
+      )}
+    </div>
   );
-}
+};
